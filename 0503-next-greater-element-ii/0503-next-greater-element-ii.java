1c4fd3f13a4a31e892da270e21b18478c26eaa83
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-07-21 09:52:10 +0530

    Time: 13 ms (74.03%), Space: 48.5 MB (60.26%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] nextGreaterElements(int[] arr) {
         int[] nge = new int[arr.length];
-        for(int i=0 ; i<arr.length ; i++){
-            nge[i] = find(arr , i);
+        int n = arr.length;
+        Stack<Integer> st = new Stack();
+        for(int i = 2*n-1 ; i >= 0 ; i--){
+            while(!st.isEmpty() && st.peek() <= arr[i%n]){
+                st.pop();
+            }
+            if(i<n){
+                nge[i] = st.isEmpty() ? -1 : st.peek();
+            }
+            st.push(arr[i%n]);
         }
         return nge;
-    }
-    public int find(int[] arr,int i){
-        for(int count=1 ; count < arr.length ; count++){
-            int idx = (i+count) % arr.length;
-            if(arr[idx] > arr[i]){
-                return arr[idx];
-            }
-        }
-        return -1;
     }
 }
