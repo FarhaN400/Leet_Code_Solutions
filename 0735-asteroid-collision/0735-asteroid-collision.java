5088d848ee93539f6e5622bcb3bad0061e573f8f
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-07-22 10:45:28 +0530

    Time: 2 ms (98.16%), Space: 46.9 MB (72.43%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] asteroidCollision(int[] arr) {
+        ArrayList<Integer> st = new ArrayList<>();
+        for(int i=0 ; i < arr.length ; i++){
+            if(arr[i] > 0) st.add(arr[i]);
+            else{
+                while(!st.isEmpty() && st.get(st.size() - 1) < Math.abs(arr[i]) && st.get(st.size() - 1) > 0 ){
+                    st.remove(st.size() - 1);
+                }
+                if(!st.isEmpty() && st.get(st.size() - 1) == Math.abs(arr[i])){
+                    st.remove(st.size() - 1);
+                }else if(st.isEmpty() || st.get(st.size() - 1) < 0){
+                    st.add(arr[i]);
+                }
+            }
+        }
+        int[] ans = new int[st.size()];
+        for(int i=0 ; i<st.size() ;i++){
+            ans[i] = st.get(i);
+        }
+        return ans;
+    }
+}
