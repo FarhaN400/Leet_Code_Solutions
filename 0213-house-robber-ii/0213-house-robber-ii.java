b7bc08cec8268e7c3bac1f23abfc1104cde70bf5
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-08-02 16:43:48 +0530

    Time: 0 ms (100%), Space: 42.5 MB (90.63%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length == 1) return nums[0];
-        int[] arr1 = Arrays.copyOfRange(nums, 0, nums.length - 1);
-        int[] arr2 = Arrays.copyOfRange(nums, 1, nums.length);
 
-        int[] dp1 = new int[arr1.length];
+        int[] dp1 = new int[nums.length];
         Arrays.fill(dp1,-1);
-        int[] dp2 = new int[arr2.length];
+        int[] dp2 = new int[nums.length];
         Arrays.fill(dp2,-1);
-        
-        int case1 =  find(0 , arr1, dp1);
-        int case2 =  find(0 , arr2, dp2);
+
+        int case1 =  find(0 , nums.length - 2 , nums, dp1);
+        int case2 =  find(1 , nums.length - 1 , nums , dp2);
         return Math.max(case1 , case2);
     }
-    public int find(int i , int[] arr, int[] dp){
-        if(i >= arr.length) {
+    public int find(int i , int end , int[] arr, int[] dp){
+        if(i > end) {
             return 0;
         }
         if(dp[i] != -1){
             return dp[i];
         }
-        int take = arr[i] + find(i+2 , arr , dp);
-        int skip = find(i+1 , arr , dp);
+        int take = arr[i] + find(i+2, end , arr , dp);
+        int skip = find(i+1 , end ,arr , dp);
         dp[i] =  Math.max(take , skip);
         return dp[i];
     }
