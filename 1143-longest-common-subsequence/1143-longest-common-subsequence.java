28d0aa9ede686d513e46d141e967590c9615415d
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-08-07 15:31:53 +0530

    Time: 24 ms (34.06%), Space: 53.9 MB (70.14%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,10 +1,27 @@
 class Solution {
     public int longestCommonSubsequence(String str1, String str2) {
-        int[][] dp = new int[str1.length()][str2.length()];
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n+1][m+1];
         for(int[] row : dp){
             Arrays.fill(row , -1);
         }
-        return helper(str1.length() - 1 , str2.length() - 1 , str1 ,str2 , dp);
+        for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;
+        for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j<= m ; j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(
+                        dp[i-1][j] ,
+                        dp[i][j-1]
+                    );
+                }
+            }
+        }
+        return dp[n][m];
     }
     public int helper(int i , int j, String str1, String str2, int[][] dp){
         if(i <0 || j < 0){
