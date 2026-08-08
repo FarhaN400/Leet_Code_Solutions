57c890153e5c7e53cef36235b201fbca2e683976
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-08-08 19:48:32 +0530

    Time: 23 ms (69.28%), Space: 54.1 MB (24.05%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -3,11 +3,6 @@ class Solution {
         int n = str1.length();
         int m = str2.length();
         int[][] dp = new int[n + 1][m + 1];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
-        }
-        for(int j = 0 ; j <= m ; j++) dp[0][j] = 0;
-        for(int i = 0 ; i <= n ; i++) dp[i][0] = 0;
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= m ; j++){
                 if(str1.charAt(i-1) == str2.charAt(j-1)){
