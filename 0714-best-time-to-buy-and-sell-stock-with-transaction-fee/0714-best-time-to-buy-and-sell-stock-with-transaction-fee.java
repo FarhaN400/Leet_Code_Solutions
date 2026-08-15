6370637d7ea400f1eaa311bb34fd454d8c58dfcf
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-08-15 14:09:01 +0530

    Time: 18 ms (43.31%), Space: 70.6 MB (45.78%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,10 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int[][] dp = new int[prices.length+1][2];
-        for(int[] row : dp){
-            Arrays.fill(row , -1);
+        // for(int[] row : dp){
+        //     Arrays.fill(row , -1);
+        // }
+        // return helper(0 , 0 , fee , prices , dp);
+        for(int i = prices.length - 1 ; i >= 0 ; i--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                if(buy==0){
+                    dp[i][buy] = Math.max(
+                        -prices[i] + dp[i+1][1],
+                        dp[i+1][0]
+                    );
+                }
+                else{
+                    dp[i][buy] = Math.max(
+                        prices[i] - fee + dp[i+1][0],
+                        dp[i+1][1]
+                    );
+                }
+            }
         }
-        return helper(0 , 0 , fee , prices , dp);
+        return dp[0][0];
     }
     public int helper(int i , int buy ,int fee, int[] arr , int[][] dp){
         int profit = 0;
