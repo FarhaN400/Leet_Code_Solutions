bcd52680b8756696d15f0055626696e72646e93e
Farhan Akhtar <imfarhan.kol@gmail.com>
2026-08-21 08:55:20 +0530

    Time: 44 ms (18.46%), Space: 46.8 MB (6.77%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1,1);
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i] > nums[j] && 1 + dp1[j] > dp1[i]){
+                    dp1[i] = 1 + dp1[j];
+                }
+            }
+        }
+
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2,1);
+        int max = 0;
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = n-1 ; j > i  ; j--){
+                if(nums[i] > nums[j] && 1 + dp2[j] > dp2[i]){
+                    dp2[i] = 1 + dp2[j];
+                }
+            }
+            if (dp1[i] > 1 && dp2[i] > 1) {
+                max = Math.max(max, dp1[i] + dp2[i] - 1);
+            }
+        }
+        return n - max;
+    }
+}
